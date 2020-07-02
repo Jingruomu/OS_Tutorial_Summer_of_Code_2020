2046c621859b0845af6c8f88e055a9398322726b
Jingruomu <41274671+Jingruomu@users.noreply.github.com>
2020-07-03 01:02:11 +0800

    Add files via upload

---
+++ Learn python The Hard Way by using python/ex1.rs
@@ -0,0 +1,17 @@
+
+fn main() {
+    println!( "I like typing this.");
+    println!( "This is fun.");
+    println!( "Ya! Printing.");
+    println!( "I'd much rather you 'not'.");
+    println!( "I \"said\" do not touch this.");
+    println!( "Yay, I get to write one more line.");
+    /*
+       println( "I like typing this.");
+       println( "This is fun.");
+       println( "Ya! Printing.");
+       println( "I'd much rather you 'not'.");
+       println( "I \"said\" do not touch this.");
+       println( "Yay, I get to write one more line.");
+    */
+}
